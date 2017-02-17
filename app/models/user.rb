0449b91fe0acd7c4b5a89dd2d4718998f263ebbe
Jacob Hixon <jacobhixon47@gmail.com>
2017-02-17 09:46:41 -0800

    4 validation specs passing for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :admin, :presence => true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+    username('joedirt25')
+    email('joedirt25@nascar.com')
+    password('joedirt12345')
+    admin(false) 
   end
 end
